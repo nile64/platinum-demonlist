f6d15431d3661e46aa3ec3fc063cecd44d5b38e4
nile64 <87245996+Nile64@users.noreply.github.com>
2024-10-14 17:09:40 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -22,7 +22,7 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container">
-                <select onchange="ReloadList();" v-model="listType" name="listselectiondropdown" id="listselectiondropdown">
+                <select onchange="await ReloadList();" v-model="listType" name="listselectiondropdown" id="listselectiondropdown">
                     <option value="demonlist" selected="selected">Demonlist</option>
                     <option value="challengelist">Challenge List</option>
                 </select>
