12c86795a3f4273e28aa0936bdbcc214eedc177b
nile64 <87245996+Nile64@users.noreply.github.com>
2024-10-14 16:59:24 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -158,6 +158,6 @@ export async function fetchLeaderboard() {
   return [res.sort((a, b) => b.total - a.total), errs];
 }
 
-export function refreshLists(){
+export async function refreshLists(){
   console.log("refreshing")
 }
